4f3d87e631e004eb042dcdb33c317cc54f611ec5
masa1029 <masaco1290@gmail.com>
2018-07-04 11:13:25 +0900

    Initial commit

---
+++ app/src/main/java/com/masaki/servicecsample/SoundManageService.java
@@ -0,0 +1,101 @@
+package com.masaki.servicecsample;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import java.io.IOException;
+
+public class SoundManageService extends Service {
+
+    private MediaPlayer _player;
+
+    @Override
+    public void onCreate() {
+        _player = new MediaPlayer();
+        String id = "soundmanagerservice_notification_channel";
+        String name = getString(R.string.notification_channel_name);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String mediaFileUriStr = "android.resource://" + getPackageName() + "/" + R.raw.a;
+        Uri mediaFileUri = Uri.parse(mediaFileUriStr);
+        try {
+            _player.setDataSource(SoundManageService.this, mediaFileUri);
+            _player.setOnPreparedListener(new PlayerPreparedListener());
+            _player.setOnCompletionListener(new PlayerCompletionListener());
+            _player.prepareAsync();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if(_player.isPlaying()) {
+            _player.stop();
+        }
+        _player.release();
+        _player = null;
+    }
+
+    private class PlayerPreparedListener implements MediaPlayer.OnPreparedListener {
+
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            mp.start();
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(SoundManageService.this, "soundmanagerservice_notification_channel");
+            builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+            builder.setContentTitle(getString(R.string.msg_notification_title_start));
+            builder.setContentText(getString(R.string.msg_notification_text_start));
+
+            Intent intent = new Intent(SoundManageService.this, SoundStartActivity.class);
+            intent.putExtra("fromNotification", true);
+            PendingIntent stopServiceIntent = PendingIntent.getActivity(SoundManageService.this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            builder.setContentIntent(stopServiceIntent);
+            builder.setAutoCancel(true);
+            Notification notification = builder.build();
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(1, notification);
+        }
+    }
+
+
+    private class PlayerCompletionListener implements MediaPlayer.OnCompletionListener {
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(SoundManageService.this, "soundmanagerservice_notification_channel");
+            builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+            builder.setContentTitle(getString(R.string.msg_notification_title_finish));
+            builder.setContentText(getString(R.string.msg_notification_text_finish));
+            Notification notification = builder.build();
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, notification);
+            stopSelf();
+        }
+    }
+}
---
+++ app/src/main/java/com/masaki/servicecsample/SoundStartActivity.java
@@ -0,0 +1,43 @@
+package com.masaki.servicecsample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SoundStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_start);
+
+        Intent intent = getIntent();
+        boolean fromNotification = intent.getBooleanExtra("fromNotification", false);
+        if(fromNotification) {
+            Button btPlay = findViewById(R.id.btPlay);
+            Button btStop = findViewById(R.id.btStop);
+            btPlay.setEnabled(false);
+            btStop.setEnabled(true);
+        }
+    }
+
+    public void onPlayButtonClick(View view) {
+        Intent intent = new Intent(SoundStartActivity.this, SoundManageService.class);
+        startService(intent);
+        Button btPlay = findViewById(R.id.btPlay);
+        Button btStop = findViewById(R.id.btStop);
+        btPlay.setEnabled(false);
+        btStop.setEnabled(true);
+    }
+
+    public void onStopButtonClick(View view) {
+        Intent intent = new Intent(SoundStartActivity.this, SoundManageService.class);
+        stopService(intent);
+        Button btPlay = findViewById(R.id.btPlay);
+        Button btStop = findViewById(R.id.btStop);
+        btPlay.setEnabled(true);
+        btStop.setEnabled(false);
+    }
+}
